446b22314b413aac58ad52a3a6a3c8e1b6563c28
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-02-09 13:15:38 -0500

    Starting the game

---
+++ app/scripts/models.js
@@ -0,0 +1,42 @@
+
+/***********************************
+//create a character constructor
+***********************************/
+
+function Character() {
+
+//create defaults
+
+//characters should all have health
+
+}
+
+/***********************************
+//create heroes
+***********************************/
+
+function Hero() {
+  // hero should have name
+  // health variable?? or just same as default??
+  // weapons
+  // damage capability
+  // alternate damage or spell capability
+  // image
+  // possible weakness??
+
+}
+
+
+/***********************************
+//create villians
+***********************************/
+
+function Villian() {
+  //villian should have name
+  //health variable?? or same as default??
+  //weapons
+  //damage capability
+  //alternate damage or spell capaiblity
+  //image
+  //possible weakness
+}
