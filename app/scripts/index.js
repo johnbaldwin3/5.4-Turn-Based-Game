7016b7d9ae75b14f6f18c77a775af659b18638ac
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-02-09 14:42:08 -0500

    commented to do list for main index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,79 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 
-// create 
+/***********************************
+//game start
+***********************************/
+
+//start game
+  //select button and direct to new page
+  //cue music?
+  //direct to player selection screen
+    //click button to go
+/***********************************
+//player select
+***********************************/
+
+//select player page
+  //dropdown or panel page
+  //select player chosen by user
+
+//************
+
+//after player is chosen
+  //randomly generate enemy
+
+//direct player to fight stage
+
+/***********************************
+//begin game
+***********************************/
+
+//begin game on go button
+
+//display player/hero
+  //display health
+
+//display the enemy
+  //display some idea/representation of health
+
+/***********************************
+//Fight, Fight, Fight!
+***********************************/
+
+//show player's fight options
+    //spell, weapon, whatever
+
+//player must select fight action
+  //after player selects action
+
+//begin attack
+  //set some interval timeout to prevent immediate attack by enemy
+
+//display some representation of attack
+  //alter enemy health
+  //determine if enemy is dead or alive
+  //return estimate of damage done to enemy
+  //end game if enemy dead....
+    //go to game over/play again screen
+  //else enemy is not dead.... (enemy attack)
+
+
+//enemy automatically attacks
+  //(pick type of attack and damage randomly)
+  //some delay to display dmg to player/hero
+  //change health of hero dependant upon attack
+  //if hero/player is dead...
+    //go to end game/play again screen
+  // else proceed to players turn
+
+//return to user options for next attack
+
+/***********************************
+//Game Over / Play Again
+***********************************/
+
+//display winner and loser
+// prompt user to play again
+// show stats?
+// on play again, take user back to selection screen
+// on end game, take player to start/splash page
