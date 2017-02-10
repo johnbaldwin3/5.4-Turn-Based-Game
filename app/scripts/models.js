d57f7d0b9f007180eeff9658b01954fb85f70643
Dakota Havel <dakotahavel@gmail.com>
2017-02-10 11:42:47 -0500

    added char images

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -60,12 +60,16 @@ goodguy.name = 'good guy';
 // Hero's List
 var jock = Hero.new();
 jock.name = "Brad";
+jock.image = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=0ahUKEwiY09as-4XSAhUKyoMKHRWnAf0QjRwIBw&url=https%3A%2F%2Ftwitter.com%2Fjaredbotha&psig=AFQjCNFIV9UxelQfhKReJT22lCkMugWfnw&ust=1486831021537503"
 var rebel = Hero.new();
 rebel.name = "James"
+rebel.image = "http://blog.spoongraphics.co.uk/wp-content/uploads/2013/04/pixel-character.jpg"
 var prep = Hero.new();
 prep.name = "Tiffany";
+prep.image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2Foriginals%2Ff5%2F04%2F6c%2Ff5046cdd7f0aaa7bd1e5a6a5a4077caf.jpg&imgrefurl=https%3A%2F%2Fuk.pinterest.com%2Fpin%2F279223245618096969%2F&docid=WgEFABijoTHnkM&tbnid=mbhpHzk855IvCM%3A&vet=1&w=500&h=500&bih=659&biw=1280&q=8%20bit%20girl%20image&ved=0ahUKEwj51dqr_IXSAhWJiFQKHT6MAPoQMwgdKAEwAQ&iact=mrc&uact=8"
 var nerd = Hero.new();
 nerd.name = "Dexter";
+nerd.image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.abeautifulsite.net%2Fcontent%2Fuploads%2F2014%2F08%2Fbit-face.png&imgrefurl=https%3A%2F%2Fwww.abeautifulsite.net%2Fhow-to-generate-8-bit-avatars&docid=_cQ-0D6BqxxXiM&tbnid=Oy6crqgQ8a3_PM%3A&vet=1&w=768&h=768&bih=659&biw=1280&q=8%20bit%20character%20image&ved=0ahUKEwiD86Od_IXSAhUjwVQKHSFUA7M4ZBAzCCooKDAo&iact=mrc&uact=8"
 
 var herosArray = [jock,rebel,prep,nerd];
 
